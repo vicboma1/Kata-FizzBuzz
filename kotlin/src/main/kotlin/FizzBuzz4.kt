a988ecc2e4b5dc6c55dc97bb732e15febe4f7d98
Victor Bolinches <vicboma1@users.noreply.github.com>
2015-11-25 20:22:04 +0100

    Create FizzBuzz4.kt

---
+++ kotlin/src/main/kotlin/FizzBuzz4.kt
@@ -0,0 +1,35 @@
+class FizzBuzz4(){
+
+    companion object {
+        val FIZZ = "Fizz"
+        val BUZZ = "Buzz"
+        val FIZZBUZZ = "$FIZZ$BUZZ"
+        val CARRY = "\n"
+        val ZERO = 0
+        val FIVE = 5
+        val THREE = 3 
+        val FIFTEEN = FIVE * THREE
+        val INIT_RANGE = 1
+        val END_RANGE_INCLUSIVE = 100
+    }
+
+    init {
+
+    }
+
+    public fun Int.mapToFizzBuzz() = mapOf(
+                      ZERO                      to this,
+                      isMultiple(this, THREE)   to FIZZ,
+                      isMultiple(this, FIVE)    to BUZZ,
+                      isMultiple(this, FIFTEEN) to FIZZBUZZ)[ZERO].toString()+"$CARRY"
+
+    public fun execute(sb : StringBuilder) : String {
+        (INIT_RANGE..END_RANGE_INCLUSIVE).forEach {
+            sb.append(it.mapToFizzBuzz())
+        }
+
+        return sb.toString()
+    }
+
+    private fun isMultiple(a : Int, b :Int) = (a % b)
+}
